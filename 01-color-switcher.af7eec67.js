0592f6c187bae240bcad4c8c6f33d877aa555a22
YarKram <104921473+YarKram@users.noreply.github.com>
2022-12-13 13:05:48 +0000

    Deploying to gh-pages from @ YarKram/goit-js-hw-09@122e0e97b61c848323aa78e26dbd3dcad0ea0f2a 🚀

---
+++ 01-color-switcher.af7eec67.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body;function o(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}var a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n.style.backgroundColor=o(),a=setInterval((function(){n.style.backgroundColor=o()}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.af7eec67.js.map
